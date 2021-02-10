29abc1246e94f7c12d523606d098169455b4095c
daniel <danieljustino79@gmail.com>
2021-02-10 15:23:25 -0300

    controller 1

---
+++ src/main/java/com/ct/api/controllers/UserController.java
@@ -0,0 +1,15 @@
+package com.ct.api.controllers;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/user")
+public class UserController {
+
+    @GetMapping()
+    public String users(){
+        return "users";
+    }
+}
